0a409a21d59f2511f8358b4e4fc837ec2f15a315
kartiknarwal <124709774+kartiknarwal@users.noreply.github.com>
2024-08-04 12:25:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+export const server ='http://localhost:4000';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
